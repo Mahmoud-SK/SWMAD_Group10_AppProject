72edf64ebdb0d41df55a74641b5fe2cc595e9c7a
yuhu0006 <yuhu0006@gmail.com>
2022-04-28 18:01:53 +0200

    Update Activity: ProfileActivity, MemeBuilderActivity Update button: a test button for ProfileActivity in MainActivity Minifest: Update camera-permission

--- app/src/main/java/com/example/swmad_group10_appproject/MainActivity.java
+++ app/src/main/java/com/example/swmad_group10_appproject/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.swmad_group10_appproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.swmad_group10_appproject.ProfileModule.ProfileActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_testProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_testProfile = findViewById(R.id.btn_testProfile);
+        btn_testProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
